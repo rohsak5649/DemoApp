639a6ab5612ee7c32269e265ff2ee254b8b57f0b
Rohan Sakhare <rohanavinashsakharefr@gmail.com>
2025-01-11 17:13:53 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Rohan Sakhare on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
